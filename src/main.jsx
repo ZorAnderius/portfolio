f9a97e904b9b440028017fe7b64794211e52acd1
ZorAnderius <gabriel.zorander@gmail.com>
2025-10-04 16:47:45 +0100

    add: font and reset styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './assets/css/reset.css'
+import './assets/css/font.css'
+import './assets/css/variables.css'
 import './index.css'
 import App from './App.jsx'
 
